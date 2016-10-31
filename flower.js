4dbef8a093f3b3a4b45e201245e14fbcb54590c5
Sumant Nidagundi <sumantnidagundi525@gmail.com>
2016-10-31 10:09:16 +0530

    Update flower.js

--- flower.js
+++ flower.js
@@ -24,7 +24,7 @@ function Flower(x, y) {
     if(this.y >= 600){
       console.log("Game over");
       textSize(24);
-            text("Oops! Game Over",380,300)
+            text("Oops! Game Over",390,300)
       
     }
 
